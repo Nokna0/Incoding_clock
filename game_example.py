2f3f15ca6cb6c68b01e32459201f7b1607fa891e
Nokna <118613153+Nokna0@users.noreply.github.com>
2024-07-05 15:09:36 +0900

    Update game_example.py

--- game_example.py
+++ game_example.py
@@ -1,47 +1,55 @@
-# Import a library of functions called 'pygame'
+# 'pygame' 라이브러리의 함수들을 가져옵니다.
 import pygame
-# Initialize the game engine
+
+# 게임 엔진을 초기화합니다.
 pygame.init()
 
-# Define the colors we will use in RGB format
-BLACK = (  0,   0,   0)
-WHITE = (255, 255, 255)
-BLUE  = (  0,   0, 255)
-GREEN = (  0, 255,   0)
-RED   = (255,   0,   0)
+# 사용할 RGB 형식으로 색상을 정의합니다.
+BLACK = (  0,   0,   0)   # 검정색
+WHITE = (255, 255, 255)  # 흰색
+BLUE  = (  0,   0, 255)  # 파란색
+GREEN = (  0, 255,   0)  # 초록색
+RED   = (255,   0,   0)  # 빨간색
  
-# Set the height and width of the screen
+# 화면의 너비와 높이를 설정합니다.
 size   = [512, 512]
-screen = pygame.display.set_mode(size)
+SCREEN = pygame.display.set_mode(size)
   
+# 창의 제목을 설정합니다.
 pygame.display.set_caption("BOOOOOOM")
   
-#Loop until the user clicks the close button.
+# 사용자가 창을 닫을 때까지 반복합니다.
 done = False
 clock = pygame.time.Clock()
 
 while not done:
   
-    # This limits the while loop to a max of 10 times per second.
-    # Leave this out and we`` will use all CPU we can.
+    # 이 코드는 초당 최대 10번 반복됩니다.
+    # 이 부분을 빼면 가능한 CPU를 모두 사용합니다.
     clock.tick(10)
-    
-    # Main Event Loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
+
+    # 메인 이벤트 루프
+    for event in pygame.event.get(): # 사용자 입력 확인
+        if event.type == pygame.QUIT: # 사용자가 창을 닫으면
+            done=True # done 변수를 True로 설정하여 루프를 종료합니다.
   
-    # All drawing code happens after the for loop and but
-    # inside the main while done==False loop.
+    # 모든 그리기 코드는 for 루프 이후, 그리고
+    # 메인 루프인 while done==False 내부에서 실행됩니다.
       
-    # Clear the screen and set the screen background
-    screen.fill(BLACK)
+    # 화면을 지우고 배경을 설정합니다.
+    SCREEN.fill(BLACK)
     
-    pygame.draw.rect(screen, WHITE, [64, 64, 384, 384], 5)
-    pygame.draw.line(screen, WHITE, [64, 128], [448-5, 128], 5)
-    pygame.draw.line(screen, WHITE, [128, 128], [128, 448-5], 5) # x좌표 절반으로 낮추기 (그냥 알아서 알아들어)
+    # 게임 화면 그리기
+    pygame.draw.rect(SCREEN, WHITE, [64, 64, 384, 384], 5)
+    pygame.draw.line(SCREEN, WHITE, [64, 128], [448-5, 128], 5)
+    pygame.draw.line(SCREEN, WHITE, [96, 128], [96, 448-5], 5)
+    pygame.draw.line(SCREEN, WHITE, [416, 128], [416, 448-5], 5)
+
+    for i in range(15):
+        grid_y = 128 - (i * 0) # 128~448 좌표 사이의 격자 생성
+        pygame.draw.line(SCREEN, WHITE, [64, 128], [448-5, 128], 5)
 
 
-    # Go ahead and update the screen with what we've drawn.
-    # This MUST happen after all the other drawing commands.
+    # 화면에 그린 내용을 업데이트합니다.
+    # 다른 그리기 명령들이 모두 실행된 후에 이 부분이 실행되어야 합니다.
     pygame.display.flip()
